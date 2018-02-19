905b4c8621cb1bec9800d73b74be70381f50007a
Vadim Kilibaba <vkulibaba@surfy.ru>
2018-02-19 13:01:09 +0300

    init

---
+++ slowpoke.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var counters sync.Map
+
+func main() {
+	var once sync.Once
+	onceBody := func() {
+		fmt.Println("Only once")
+	}
+	done := make(chan bool)
+	for i := 0; i < 10; i++ {
+		go func() {
+			once.Do(onceBody)
+			done <- true
+		}()
+	}
+	for i := 0; i < 10; i++ {
+		<-done
+	}
+}
