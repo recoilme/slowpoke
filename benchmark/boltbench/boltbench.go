4e4eef44979cb8f7063a23d8b85ff17c6aa12dfc
Vadim Kilibaba <vkulibaba@surfy.ru>
2018-03-12 18:20:24 +0300

    bench

--- benchmark/boltbench/boltbench.go
+++ benchmark/boltbench/boltbench.go
@@ -2,26 +2,29 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"sync"
 	"time"
 
 	"github.com/boltdb/bolt"
-	"github.com/recoilme/slowpoke"
 )
 
 var boltdb *bolt.DB
 
 func main() {
+	file := "1.db"
+	var err error
+	os.Remove(file)
+	boltdb, err = bolt.Open(file, 0600, &bolt.Options{Timeout: 1 * time.Second})
+	if err != nil {
+		fmt.Printf("%s\n", err)
+	}
 	testSet()
 
 }
 
 func testSet() {
 	var err error
-	boltdb, err = bolt.Open("2.db", 0600, &bolt.Options{Timeout: 1 * time.Second})
-	if err != nil {
-		fmt.Printf("%s\n", err)
-	}
 	var wg sync.WaitGroup
 
 	append := func(i int) {
@@ -47,11 +50,37 @@ func testSet() {
 		append(i)
 	}
 	wg.Wait()
-	boltdb.Close()
+
 	t2 := time.Now()
 
 	fmt.Printf("The 100 Set took %v to run.\n", t2.Sub(t1))
-	slowpoke.CloseAll()
+
+	read := func(i int) {
+		defer wg.Done()
+		k := []byte(fmt.Sprintf("%04d", i))
+		bucketstr := "1"
+		boltdb.View(func(tx *bolt.Tx) error {
+			b := tx.Bucket([]byte(bucketstr))
+			if b == nil {
+				return nil
+			}
+			_ = b.Get(k)
+			return nil
+		})
+	}
+
+	t3 := time.Now()
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		read(i)
+	}
+	wg.Wait()
+	t4 := time.Now()
+
+	fmt.Printf("The 100 Get took %v to run.\n", t4.Sub(t3))
+	boltdb.Close()
 }
 
-//The 10000 Set took 1.650810209s to run.
+//Macbook 2017
+//The 100 Set took 15.538641ms to run.
+//The 100 Get took 191.673µs to run.
--- benchmark/slowpokebench/slowpokebench.go
+++ benchmark/slowpokebench/slowpokebench.go
@@ -39,7 +39,28 @@ func testSet() {
 	t2 := time.Now()
 
 	fmt.Printf("The 100 Set took %v to run.\n", t2.Sub(t1))
+
+	read := func(i int) {
+		defer wg.Done()
+		k := []byte(fmt.Sprintf("%04d", i))
+
+		_, _ = slowpoke.Get(file, k)
+
+	}
+
+	t3 := time.Now()
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		read(i)
+	}
+	wg.Wait()
+	t4 := time.Now()
+
+	fmt.Printf("The 100 Get took %v to run.\n", t4.Sub(t3))
+
 	slowpoke.CloseAll()
 }
 
-//The 10000 Set took 1.650810209s to run.
+//macbook 2017
+//The 100 Set took 13.270801ms to run.//15.538641ms
+//The 100 Get took 279.128µs to run.//191.673µs to run.
