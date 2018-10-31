7864937631e2daf466ac1b4d4f3a6efc190b5f0a
recoilme <vadim.kulibaba@gmail.com>
2018-10-31 23:05:17 +0300

    memstorage

--- newsp/newsp.go
+++ newsp/newsp.go
@@ -20,19 +20,30 @@ func init() {
 }
 
 func main() {
-	e := set("", int8(1), int8(2), int8(3), int8(4))
+	e := set("", 1, 2, 3, 4, "5", "6")
 	log.Println(e)
-	get("", 1)
+	var a []byte
+	a, _ = get("", 1)
+	log.Println(a)
+	a, _ = get("", 3)
+	log.Println(a)
+	a, _ = get("", "54")
+	log.Println(a)
+	if a == nil {
+		log.Println("nil")
+	}
 }
 
 func toBinary(v interface{}) ([]byte, error) {
 	var err error
 	buf := new(bytes.Buffer)
 	switch v.(type) {
-	case bool, float32, float64, complex64, complex128, int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64, []byte:
+	case bool, float32, float64, complex64, complex128, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64, []byte:
 		err = binary.Write(buf, binary.BigEndian, v)
+	case int:
+		err = binary.Write(buf, binary.BigEndian, int64(v.(int)))
 	case string:
-		_, err = buf.Write([]byte(string(v.(string))))
+		_, err = buf.Write([]byte((v.(string))))
 	default:
 		enc := gob.NewEncoder(buf)
 		err = enc.Encode(v)
@@ -40,48 +51,39 @@ func toBinary(v interface{}) ([]byte, error) {
 	return buf.Bytes(), err
 }
 
-func set(f string, params ...interface{}) (err error) {
+func set(f string, params ...interface{}) error {
+	var e error
 	if f == "" {
 		mem.Lock()
 		defer mem.Unlock()
-
 		for k, v := range params {
 			if k%2 != 0 {
 				key, err := toBinary(params[k-1])
 				if err != nil {
+					e = err
 					break
 				}
 				val, err := toBinary(v)
 				if err != nil {
+					e = err
 					break
 				}
-				log.Printf("%+v", mem.kv)
 				mem.kv[string(key)] = val
-				log.Printf("%+v", mem.kv)
 			}
 		}
-		for key, val := range mem.kv {
-			log.Println("11111", key, val)
-		}
-		//log.Printf("%+v", mem.kv)
 	}
-	return err
+	return e
 }
 
-func get(f string, k interface{}) (err error) {
-
+func get(f string, k interface{}) (v []byte, err error) {
 	if f == "" {
 		mem.RLock()
 		defer mem.RUnlock()
 		key, err := toBinary(k)
-		//log.Println(key, err)
 		if err != nil {
-			return err
+			return nil, err
 		}
-		//log.Println("333")
-		v := mem.kv[string(key)]
-		_ = v
-		log.Println(v)
+		v = mem.kv[string(key)]
 	}
-	return err
+	return v, err
 }
